db101f713084b198010db570e878de6ac7f0c5d2
martin-bod <78853979+martin-bod@users.noreply.github.com>
2021-05-30 14:28:08 +0200

    Add files via upload

--- inscription.php
+++ inscription.php
@@ -9,34 +9,52 @@ $database = "inscription";
 
 
 //connection a la BDD
-$connection = mysqli_connect('localhost','root','');
-$bdd = mysqli_select_db($connection,$database);
+$db_handle = mysqli_connect('localhost','root','root');
+$db_found = mysqli_select_db($db_handle,$database);
 
 
 
 //récupérer les données venant du formulaire
+$email = $_POST['email'];
 $nom = $_POST['nom'];
-$prenom = $_POST['prenom'];
-$pseudo = $_POST['pseudo']; 
-$email = $_POST['email']);
-$password = $_POST['password'];
+$prenom = $_POST['prenom']; 
+$naissdate = $_POST['naissdate'];
+$mdp = $_POST['mdp'];
+$adresse1 = $_POST['adresse1'];
+$adresse2 = $_POST['adresse2'];
+$ville = $_POST['ville'];
+$codepostal = $_POST['codepostal'];
+$pays = $_POST['pays'];
+$telephone = $_POST['telephone'];
 $type = $_POST['type'];
 
-
-
-//saisi
-$sql = "INSERT INTO inscriptionECEMarketPlace (Pseudo, Nom, Prénom, Email, Mdp, Type)
-VALUES('$pseudo', '$nom', '$prenom', '$email', 'password', 'type')";
-
-//Put in database
-$result = mysqli_query($connection, $sql);
-
-if($result){
-    echo "Inscription réussi";
+if($type == "Vendeur"){
+    $user = 0;
 }else{
-    echo "Fail";
+    $user = 1;
 }
 
-mysqli_close($connection);
+
+if($db_found){
+    //saisi
+    $sql = "INSERT INTO inscriptionECEMarketPlace (email, nom, prenom, naissdate, mdp, adresse1, adresse2, ville, codepostal, pays, telephone, type) VALUES('$email', '$nom', '$prenom', '$naissdate', '$mdp', '$adresse1', '$adresse2', '$ville', '$codepostal', '$pays', '$telephone', '$user')";
+
+    $result = mysqli_query($db_handle, $sql);
+    
+
+    if($result){
+        echo "Inscription réussi";
+    }
+    else{
+        echo "Fail";
+    }
+
+}else{
+    echo "database not found";
+}
+
+
+
+mysqli_close($db_handle);
 
 ?>
